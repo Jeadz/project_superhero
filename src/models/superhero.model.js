ce47dd22656660f834980024c645dfd9b1d6e776
JEAD <jeadz1234@gmail.com>
2023-02-24 15:25:40 -0500

    Created a super_hero_project

---
+++ src/models/superhero.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const superhero_schema = mongoose.Schema({
+    /* Nombre, universo, superpoderes y la dirección */
+        superhero: { type: String, require: true },
+        universe: { type: String, require: true },
+        superpowes: { type: Array, require: true},
+        address: {
+            type: Object,
+            require: true,
+            city: { type: String, require: true},
+            code_zip: { type: String, require: true},
+        },
+});
+module.exports = mongoose.model("SuperheroCollection", superhero_schema);
---
+++ src/routes/superhero.routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const superhero_model = require("../models/superhero.model");
+const superhero_routes_http = express.Router();
+
+/*  Crear un nuevo superheroe
+    mongoose method: save()
+    http://localhost:5000/
+*/
+
+superhero_routes_http.post("/", (req, res) => {
+    const new_superhero = superhero_model(req.body);
+    new_superhero
+        .save()
+        .then((data) => res.json(data))
+        .catch((err) => res.json({message: err}));
+});
+
+/*  Listar todos los registros de la BD
+    mongoose method: find ()*/
+superhero_routes_http.get("/", (req, res) => {
+    superhero_model
+      .find()
+      .then((data) => res.json(data))
+      .catch((err) => res.json({message: err}));
+});
+
+/*  Consultar un usuario de forma especifica por el id
+    http: get
+    mongoose method: findById({_id: ?})
+*/
+
+superhero_routes_http.get("/:superheroId", (req, res) => {
+    const { superheroId } = req.params;
+    superhero_model
+        .findById({ _id: superheroId })
+        .then((data) => res.json(data))
+        .catch((err) => res.json({ message: err }));
+});
+
+module.exports = superhero_routes_http;
